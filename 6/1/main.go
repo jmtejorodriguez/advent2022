cf7adfb5fd5ee3a386de0d7b94ecbdda9f02c6cd
Julián Muriel Tejo Rodríguez <jmtejorodriguez@gmail.com>
2022-12-06 22:39:53 +0100

    day 6

---
+++ 6/1/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+func main() {
+	pwd, _ := os.Getwd()
+	path := filepath.Join(pwd, "6/input.txt")
+	dat, e := os.ReadFile(path)
+
+	if e != nil {
+		panic(e)
+	}
+
+	last := 0
+	for i := 1; i < len(dat); i++ {
+		for j := i - 1; j >= last; j-- {
+			if dat[j] == dat[i] {
+				last = j + 1
+				break
+			}
+		}
+		if i-last == 3 {
+			fmt.Print(i + 1)
+			break
+		}
+	}
+}
